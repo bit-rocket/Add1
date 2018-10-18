0440046040cc1ee9d142957957369a18b9eaee3f
pingtouge <ye515430@163.com>
2018-10-19 00:20:47 +0800

    full version

---
+++ Add1-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Add1-Bridging-Header.h
+//  Add1
+//
+//  Created by itrump on 2018/10/18.
+//  Copyright © 2018 fengxiaoyang. All rights reserved.
+//
+
+#ifndef Add1_Bridging_Header_h
+#define Add1_Bridging_Header_h
+
+#import <MBProgressHUD/MBProgressHUD.h>
+
+#endif /* Add1_Bridging_Header_h */
--- Add1/MainViewController.swift
+++ Add1/MainViewController.swift
@@ -7,17 +7,29 @@
 //
 
 import UIKit
+import MBProgressHUD
 
 class MainViewController: UIViewController {
     
     @IBOutlet weak var numbersLable: UILabel?
     @IBOutlet weak var scoreLabel: UILabel?
     @IBOutlet weak var inputField: UITextField?
+    @IBOutlet weak var timeLable: UILabel?
     
     var score:Int = 0
+    var hud: MBProgressHUD?
+    
+    var timer: Timer?
+    var seconds: Int = 60
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        hud = MBProgressHUD(view: self.view)
+        if hud != nil
+        {
+            self.view.addSubview(hud!)
+        }
         
         setRandomNumberLable()
         updateScoreLabel()
@@ -57,14 +69,96 @@ class MainViewController: UIViewController {
             {
                 print("Correct.")
                 score += 1
+                showHudWithAnswer(isRight: true)
             } else {
-                score -= 1
+                if score > 0
+                {
+                    score -= 1
+                }
+                showHudWithAnswer(isRight: false)
             }
         }
         
         setRandomNumberLable()
         updateScoreLabel()
         clearInput()
+        
+        if (timer == nil)
+        {
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(onUpdateTimer), userInfo: nil, repeats: true)
+        }
+        if score == 2
+        {
+            timer?.invalidate()
+            timer = nil
+            timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(onUpdateTimer), userInfo: nil, repeats: true)
+        }
+    }
+    
+    func updateTimerLable()
+    {
+        if (timeLable != nil)
+        {
+            let min: Int = (seconds / 60) % 60
+            let sec: Int = seconds % 60
+            
+            let min_p: String = String(format: "%02d", min)
+            let sec_p: String = String(format: "%02d", sec)
+            
+            timeLable?.text = "\(min_p):\(sec_p)"
+        }
+    }
+    
+    @objc func onUpdateTimer() -> Void
+    {
+        if (seconds > 0 && seconds <= 60)
+        {
+            seconds -= 1
+            updateTimerLable()
+        } else if (seconds <= 0)
+        {
+            if (timer != nil)
+            {
+                timer?.invalidate()
+                timer = nil
+                
+                let alertController = UIAlertController(title: "Time up!", message: "Your time is up! Score: \(score)", preferredStyle: .alert)
+                let restartAction = UIAlertAction(title: "Restart", style: .default, handler: nil)
+                
+                alertController.addAction(restartAction)
+                self.present(alertController, animated: true, completion: nil)
+                
+                score = 0
+                seconds = 60
+                updateTimerLable()
+                updateScoreLabel()
+                setRandomNumberLable()
+            }
+        }
+    }
+    
+    func showHudWithAnswer(isRight: Bool)
+    {
+        var imageView: UIImageView?
+        if isRight
+        {
+            imageView = UIImageView(image: UIImage(named: "thumbs-up"))
+        } else
+        {
+            imageView = UIImageView(image: UIImage(named: "thumbs-down"))
+        }
+        if (imageView != nil)
+        {
+            hud?.mode = MBProgressHUDMode.customView
+            hud?.customView = imageView
+            
+            hud?.show(animated: true)
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+                self.hud?.hide(animated: true)
+                self.inputField?.text = ""
+            })
+        }
     }
     
     func updateScoreLabel() {
@@ -74,11 +168,11 @@ class MainViewController: UIViewController {
     func clearInput() {
         inputField?.text = ""
         
-        inputField?.becomeFirstResponder()
+        print("\(String(describing: inputField?.becomeFirstResponder()))")
     }
     
     func setRandomNumberLable() {
-        numbersLable?.text = "\(generateRandomNumber())"
+        numbersLable?.text = String(format: "%04d", generateRandomNumber())
     }
     
     func generateRandomNumber() -> Int {
